f52a9bc2b18f88fc9945bde8ddca027ee7514860
Sam Bokai <sam.bokai@scout24.com>
2018-05-09 22:55:59 +0200

    Refresh upon click on navbar brand/logo

--- src/js/components/ActionBar.jsx
+++ src/js/components/ActionBar.jsx
@@ -8,7 +8,7 @@ class ActionBar extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark justify-content-between">
-        <a className="navbar-brand" href="#">Notedown</a>
+        <a className="navbar-brand" href="/">Notedown</a>
         <form className="form-inline">
           <button
             onClick={this.props.onCreateNote}
