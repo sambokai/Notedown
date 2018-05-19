20eb3e676425780b5c40938dcd0a345f851e23d7
Sam Bokai <sam.bokai@scout24.com>
2018-05-19 16:00:28 +0200

    Add snapshot test for NoteListItem

---
+++ src/js/components/noteselector/notelist/noteslistitem/NoteListItem.test.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable comma-dangle,function-paren-newline */
+
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+import renderer from 'react-test-renderer';
+
+import NoteListItem from './NoteListItem';
+
+
+const onSelectNoteMock = jest.fn();
+onSelectNoteMock.mockReturnValue('This Note was selected');
+
+const mockData = {
+  note: {
+    body: 'This is Note #1',
+    id: 1,
+    lastChange: new Date(2018, 0).valueOf()
+  },
+  draggableProvided: {},
+  onSelectNote: onSelectNoteMock
+};
+
+
+it('renders correctly', () => {
+  const wrapper = renderer
+    .create(
+      <DragDropContext>
+        <NoteListItem
+          note={mockData.note}
+          onSelectNote={mockData.onSelectNote}
+          isSelected
+          draggableProvided={mockData.draggableProvided}
+        />
+      </DragDropContext>
+    );
+
+  expect(wrapper)
+    .toMatchSnapshot();
+});
