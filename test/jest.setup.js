4d15ecdae707818fe4fcc9c5386b227d4f334349
Sam Bokai <sam.bokai@scout24.com>
2018-05-19 23:11:41 +0200

    Move jest setup script to /test folder

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,4 +1,7 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import setLocalStorageMock from './mocks/localStorageMock';
+
+setLocalStorageMock();
 
 Enzyme.configure({ adapter: new Adapter() });
