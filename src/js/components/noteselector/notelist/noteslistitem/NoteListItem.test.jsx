410575f985dc726bef1b3c68834eee26b3086f86
Sam Bokai <sam.bokai@scout24.com>
2018-05-19 16:38:34 +0200

    Add unit tests for NoteListItem's getNoteTitle() function

--- src/js/components/noteselector/notelist/noteslistitem/NoteListItem.test.jsx
+++ src/js/components/noteselector/notelist/noteslistitem/NoteListItem.test.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable comma-dangle,function-paren-newline */
 
 import React from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
 
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import NoteListItem from './NoteListItem';
 
@@ -40,4 +42,36 @@ it('renders correctly', () => {
 
   expect(wrapper)
     .toMatchSnapshot();
+});
+
+describe('getNoteTitle()', () => {
+  it("returns the first line of text in a note's body as a trimmed string", () => {
+    const testNote = {
+      ...mockData.note,
+      body: ' \n \n    This is the first line of Note #1     \n \n ' +
+      'Yesterday I went to the mall. It was great.'
+    };
+
+    const wrapper = shallow(<NoteListItem isSelected note={testNote} onSelectNote={mockData.onSelectNote} />);
+
+    const result = wrapper.instance().getNoteTitle();
+
+    expect(result).toBe('This is the first line of Note #1');
+  });
+
+  it("returns a predefined 'Empty'-Message if the note's body is empty", () => {
+    const testNote = {
+      ...mockData.note,
+      body: ''
+    };
+    const emptyNoteTitle = 'Empty';
+
+    const wrapper = shallow(
+      <NoteListItem isSelected note={testNote} onSelectNote={mockData.onSelectNote} emptyNoteTitle={emptyNoteTitle} />
+    );
+
+    const result = wrapper.instance().getNoteTitle();
+
+    expect(result).toBe(emptyNoteTitle);
+  });
 });
