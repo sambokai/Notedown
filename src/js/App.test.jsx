9ada83e3235b8d4ef2ae9786511c94dc3de7109a
Sam Bokai <sam.bokai@scout24.com>
2018-05-20 23:32:35 +0200

    Add tests for <App/>

--- src/js/App.test.jsx
+++ src/js/App.test.jsx
@@ -5,10 +5,16 @@ import { shallowToJson } from 'enzyme-to-json';
 
 import App from './App';
 
-
 describe('<App/>', () => {
-  it('should render correctly', () => {
+  it('should render correctly, when there are no notes', () => {
     const wrapper = shallow(<App />);
+
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render correctly, when notes are existing', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ notes: [{ body: 'This is a note.', id: 5 }] });
 
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
