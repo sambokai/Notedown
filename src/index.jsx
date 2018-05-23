5e6620929dbd48beb31047cbc3ff64a09dfd3b6f
Sam Bokai <sam.bokai@scout24.com>
2018-05-23 09:59:20 +0200

    Switch to react-router's BrowserRouter (HTML5 History API)
    Testing history api mode with AWS S3

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import App from './js/App';
 
