856663b6f69287ac45951acdfdf43fdad0d7b380
Sam Bokai <sam.bokai@scout24.com>
2018-05-24 01:39:36 +0200

    Try simple selenium example-test

---
+++ test/e2e/notes.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Builder, By, until } from 'selenium-webdriver';
+
+
+const rootURL = 'localhost:8080';
+let driver;
+
+const getElementTimeout = 9000;
+
+async function getElementById(id) {
+  const el = await driver.wait(until.elementLocated(By.id(id)), getElementTimeout);
+  return driver.wait(until.elementIsVisible(el), getElementTimeout);
+}
+
+beforeEach((done) => {
+  driver = new Builder().forBrowser('chrome').build();
+
+  driver.get(rootURL).then(done);
+});
+
+
+afterEach((done) => {
+  driver.quit().then(done);
+});
+
+
+describe('navbar"', () => {
+  it('should have a logo that reads "Notedown"', async () => {
+    const el = await getElementById('navbar-logo');
+
+    const expected = 'Notedown';
+    const actual = await el.getText();
+    expect(actual).toEqual(expected);
+  });
+});
+
