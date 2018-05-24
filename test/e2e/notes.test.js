9067ac90a904e1c8d7e198d8b445afe5fde3a0e4
Sam Bokai <sam.bokai@scout24.com>
2018-05-25 00:27:28 +0200

    Add first e2e test suite

--- test/e2e/notes.test.js
+++ test/e2e/notes.test.js
@@ -1,43 +1,106 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { Builder, By, Capabilities, until } from 'selenium-webdriver';
+import { Builder, By, Capabilities } from 'selenium-webdriver';
 
 import 'chromedriver';
 
 const rootURL = 'http://localhost:8080';
 let driver;
 
-const getElementTimeout = 9000;
-
-async function getElementById(id) {
-  const el = await driver.wait(until.elementLocated(By.id(id)), getElementTimeout);
-  return driver.wait(until.elementIsVisible(el), getElementTimeout);
-}
-
 const chromeCapabilities = Capabilities.chrome();
 chromeCapabilities.set('chromeOptions', { args: ['--headless', '--no-sandbox'] });
 
-beforeEach((done) => {
+const initializeDriver = (done) => {
   driver = new Builder()
     .forBrowser('chrome')
     .withCapabilities(chromeCapabilities)
     .build();
 
   driver.get(rootURL).then(done);
-});
+};
 
-
-afterEach((done) => {
+const quitDriverAndClearLocalStorage = (done) => {
+  driver.getLocalStorage().clear();
   driver.quit().then(done);
-});
+};
 
+describe('After fresh loading of the app', () => {
+  const selectors = {
+    textArea: By.id('text-editor-textarea'),
+    newNoteButton: By.id('new-note-button'),
+    noteList: By.id('note-list'),
+    noteListItem: By.id('note-list-item'),
+    noteListItemTitle: By.id('note-list-item-title'),
+  };
 
-describe('navbar"', () => {
-  it('should have a logo that reads "Notedown"', async () => {
-    const el = await getElementById('navbar-logo');
+  describe('1', () => {
+    beforeAll(initializeDriver);
+    afterAll(quitDriverAndClearLocalStorage);
 
-    const expected = 'Notedown';
-    const actual = await el.getText();
-    expect(actual).toEqual(expected);
+    test('textarea should be deactivated / non-focusable', async () => {
+      const textArea = await driver.findElement(selectors.textArea);
+      expect(await textArea.isEnabled()).toBe(false);
+    });
+
+    test('new-note button should be activated / clickable and add a note to the list', async () => {
+      const newNoteButton = await driver.findElement(selectors.newNoteButton);
+      const noteList = await driver.findElement(selectors.noteList);
+
+      newNoteButton.click();
+
+      const noteListItems = await noteList.findElements(selectors.noteListItem);
+
+      expect(noteListItems).toHaveLength(1);
+    });
+
+    test('newly created note should be selected', async () => {
+      const noteList = await driver.findElement(selectors.noteList);
+      const noteListItems = await noteList.findElements(selectors.noteListItem);
+
+      const classes = await noteListItems[0].getAttribute('class').then(classesString =>
+        classesString.trim().split(' ').map(classString => classString.trim()));
+
+      expect(classes).toContain('active');
+    });
+
+    test('new-note button should be deactivated / non-clickable ' +
+      'after having just clicked it / having an empty note', async () => {
+      const newNoteButton = await driver.findElement(selectors.newNoteButton);
+      const noteList = await driver.findElement(selectors.noteList);
+
+      newNoteButton.click();
+
+      const noteListItems = await noteList.findElements(selectors.noteListItem);
+
+      expect(noteListItems).toHaveLength(1);
+    });
+
+    test('textarea should be active / focusable', async () => {
+      const textArea = await driver.findElement(selectors.textArea);
+      expect(await textArea.isEnabled()).toBe(true);
+    });
+
+    test("selected note title should show first line of the note's body", async () => {
+      const textArea = await driver.findElement(selectors.textArea);
+      const noteList = await driver.findElement(selectors.noteList);
+      const noteListItems = await noteList.findElements(selectors.noteListItem);
+
+      textArea.click();
+      textArea.sendKeys('    This is the title of this Note. \n \n ' +
+        'Yesterday i was going down the street when suddenly a unicorn ...');
+
+      const noteListItem = await noteListItems[0].findElements(selectors.noteListItemTitle).then(elements => elements[0]);
+      const noteListItemText = await noteListItem.getText();
+
+      expect(noteListItemText).toBe('This is the title of this Note.');
+    });
+  });
+
+  describe('2', () => {
+    beforeAll(initializeDriver);
+    afterAll(quitDriverAndClearLocalStorage);
+
+    it('test 2', () => {
+      expect(2).toEqual(2);
+    });
   });
 });
 
